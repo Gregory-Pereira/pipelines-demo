f79139eae1051f0063fad6651bfdd5bd7d6cbea6
greg pereira <grpereir@redhat.com>
2023-12-12 13:28:01 -0800

    cleaning up scripts

--- 01_verify_source_code_pipeline/generate-webhook-secret-and-customize-manifests.sh
+++ 01_verify_source_code_pipeline/generate-webhook-secret-and-customize-manifests.sh
@@ -6,6 +6,8 @@ if [[ -n $webhook_secret_exists ]]; then
   if [[ -n $webhook_secret ]]; then
     oc create secret generic webhook-secret-verify-source-code-pipeline-demo -n securesign-pipelines-demo \
       --from-literal=webhook-secret-key=$webhook_secret  --dry-run=client -o yaml | oc replace -f -
+  else
+    echo "Skipping webhook secret update." 
   fi
 else
   read -s -p "Enter the secret value used to setup the Github webhook: " webhook_secret
@@ -15,72 +17,39 @@ fi
 
 read -p "Enter the case sensitive github organization and repo combination (ex: 'securesign/pipelines-demo'): " github_org_and_repo
 
-echo "apiVersion: triggers.tekton.dev/v1alpha1
-kind: EventListener
-metadata:
-  name: verify-source
-  namespace: securesign-pipelines-demo
-spec:
-  resources:
-    kubernetesResource:
-      replicas: 1
-  serviceAccountName: pipeline
-  triggers: 
-    - bindings:
-        - kind: TriggerBinding
-          ref: github-push-triggerbinding
-        - kind: TriggerBinding
-          ref: trusted-artifact-signer-triggerbinding
-        - kind: TriggerBinding
-          ref: verify-source-code-triggerbinding
-      interceptors:
-        - params:
-            - name: secretRef
-              value:
-                secretKey: webhook-secret-key
-                secretName: webhook-secret-verify-source-code-pipeline-demo
-          ref:
-            kind: ClusterInterceptor
-            name: github
-        - params:
-            - name: filter 
-              value: >-
-                (header.match('X-GitHub-Event', 'push') &&
-                body.repository.full_name == '$github_org_and_repo')
-            - name: overlays
-              value:
-                - expression: 'body.ref.split(''/'')[2]'
-                  key: ref
-          ref:
-            kind: ClusterInterceptor
-            name: cel
-      name: verify-source-code-trigger
-      template: 
-        ref: verify-source-code-triggertemplate
-" > ./verify-source-el.yaml
+check_repo_full_name=$(
+  curl -L -s \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/$github_org_and_repo \
+  | jq .full_name | cut -d "\"" -f 2
+)
+
+if [[ $check_repo_full_name != $github_org_and_repo ]]; then
+  echo "Couldnt find this repo. Some common issues that would prevent this from working:
+    1. Repo is not on github.
+    2. Repo is not private.
+    3. Repo is not listed with proper casing.
+      - To check casing you can run this command:
+        \`curl -L -s \\
+            -H "Accept: application/vnd.github+json" \\
+            -H "X-GitHub-Api-Version: 2022-11-28" \\
+            https://api.github.com/repos/$github_org_and_repo \\
+          | jq .full_name \`"
+  exit 1
+else
+  echo "repo exists, continuing."
+fi
+
+trigger_value="(header.match('X-GitHub-Event', 'push') && body.repository.full_name == '$github_org_and_repo')" \
+yq -i '.spec.triggers.0.interceptors.1.params.0.value = env(trigger_value)' ./verify-source-el.yaml 
 
 tuf_route_name=$(oc get routes -n tuf-system | grep 'tuf' | awk '{print $1}')
 tuf_route_hostname=$(oc get route -n tuf-system $tuf_route_name -o jsonpath='{.spec.host}')
 generic_route_hostname="${tuf_route_hostname:4:${#tuf_route_hostname}}"
 host="el-verify-source.$generic_route_hostname"
 
-echo "apiVersion: route.openshift.io/v1
-kind: Route
-metadata:
-  name: el-verify-source
-  namespace: securesign-pipelines-demo
-spec:
-  host: $host
-  port:
-    targetPort: http-listener
-  to:
-    kind: Service
-    name: el-verify-source
-    weight: 100
-  tls:
-    termination: edge
-    insecureEdgeTerminationPolicy: Redirect
-  wildcardPolicy: None" > ./verify-source-el-route.yaml
+yq -i '.spec.host = "'$host'"' ./verify-source-el-route.yaml
 
 echo "------------------ Keycloak User Configuration -----------------"
 read -p "Enter the username for the keycloak user (must be all lowercase letters): " keycloak_user
@@ -91,57 +60,15 @@ read -p "Please enter an email for the keycloak user: " keycloak_email
 read -p "Enter your first name: " first_name
 read -p "Enter your last name: " last_name
 
-echo "apiVersion: keycloak.org/v1alpha1
-kind: KeycloakUser
-metadata:
-  labels:
-    app: sso
-  name: $keycloak_user
-  namespace: keycloak-system
-spec:
-  realmSelector:
-    matchLabels:
-      app: sso
-  user:
-    email: $keycloak_email
-    enabled: true
-    emailVerified: true
-    credentials:
-      - type: "password"
-        value: $keycloak_pass
-    firstName: $first_name
-    lastName: $last_name
-    username: $keycloak_user" > ./keycloak-user.yaml
+yq -i '.metadata.name = "'$keycloak_user'"' ./keycloak-user.yaml
+yq -i '.spec.user.username = "'$keycloak_user'"' ./keycloak-user.yaml
+yq -i '.spec.user.email = "'$keycloak_email'"' ./keycloak-user.yaml
+yq -i '.spec.user.credentials.0.value = "'$keycloak_pass'"' ./keycloak-user.yaml
+yq -i '.spec.user.firstName = "'$first_name'"' ./keycloak-user.yaml
+yq -i '.spec.user.lastName = "'$last_name'"' ./keycloak-user.yaml
 
-search_term="amd64/linux"
-oc_download_name=$(oc get consoleclidownloads.console.openshift.io -n openshift-console | grep "oc-cli" | awk '{print $1}')
-KEYS=$(oc get consoleclidownloads.console.openshift.io $oc_download_name -n openshift-console -o json | jq '.spec.links[].href | @sh' | cut -d \" -f 2 | cut -d \' -f 2) # dont know if its called oc-cli-downloads on ever cluster
-declare download_types_arr
-download_types_arr=($KEYS)
+yq -i '.spec.params.0.value = "'$keycloak_email'"' ./verify-source-code-triggerbinding.yaml
 
-declare correct_oc_link
-counter=0
-for i in "${download_types_arr[@]}"; do
-  if [[ $i == *"${search_term}"* && -n $search_term ]]; then
-    correct_oc_link=$i
-  fi
-done
-if [[ -z $correct_oc_link ]]; then
-    echo "Unable to find correct oc download link for your architecture and OS.
-Please find the correct binary link for your OS and ARCH from the openshift console, or using the oc binary:
-    \`oc get consoleclidownloads.console.openshift.io oc-cli-downloads -n openshift-console -o json\`.
-Once you have found it, please enter the oc download link as a value in the \`./verify-source-code-triggerbinding.yaml\` file."
-fi
-
-echo "apiVersion: triggers.tekton.dev/v1alpha1
-kind: TriggerBinding
-metadata:
-  name: verify-source-code-triggerbinding
-  namespace: securesign-pipelines-demo
-spec:
-  params:
-    - name: oidc-user-email
-      value: $keycloak_email
-    - name: oc-download-link
-      value: $correct_oc_link
-" > ./verify-source-code-triggerbinding.yaml
+pipeline_sa_secrets=$(oc get sa pipeline -n securesign-pipelines-demo -o yaml | yq .secrets)
+pipeline_sa_secret_name=$(echo $pipeline_sa_secrets | grep "pipeline-dockercfg-" | yq .0.name)
+# cat verify-source-code-pipeline.yaml | yq '.spec.tasks.1.params = .spec.tasks.1.params + {"name":"test", "value":"testvalue"}
